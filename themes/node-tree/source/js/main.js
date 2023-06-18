d5af3947a683e4c23d2d2684a94681883af8b949
Exisi <exi.simat@gmail.com>
2023-06-18 14:24:01 +0800

    ✨ feat(sidebar): 支持顶层目录的全展开和全收起

--- themes/node-tree/source/js/main.js
+++ themes/node-tree/source/js/main.js
@@ -8,23 +8,24 @@ $(document).ready(function () {
 	scrollToTop();
 	pageScroll();
 	wrapImageWithFancyBox();
+	toggleAll();
 });
 
 // 页面滚动
 function pageScroll() {
-	var start_hight = 0;
+	let start_hight = 0;
 	$(window).on("scroll", function () {
-		var end_hight = $(window).scrollTop();
-		var distance = end_hight - start_hight;
+		let end_hight = $(window).scrollTop();
+		let distance = end_hight - start_hight;
 		start_hight = end_hight;
-		var $header = $("#header");
+		let $header = $("#header");
 		if (distance > 0 && end_hight > 50) {
-			$header.hide();
-		} else if (distance < 0) {
-			$header.show();
-		} else {
-			return false;
+			return $header.hide();
 		}
+		if (distance < 0) {
+			return $header.show();
+		}
+		return false;
 	});
 }
 
@@ -39,26 +40,25 @@ function scrollToTop() {
 function switchTreeOrIndex() {
 	$("#sidebar-toggle").on("click", function () {
 		if ($("#sidebar").hasClass("on")) {
-			scrollOff();
-		} else {
-			scrollOn();
+			return scrollOff();
 		}
+		scrollOn();
 	});
 	$("body").click(function (e) {
 		if (window.matchMedia("(max-width: 1100px)").matches) {
-			var target = $(e.target);
-			if (!target.is("#sidebar *")) {
-				if ($("#sidebar").hasClass("on")) {
-					scrollOff();
-				}
+			let target = $(e.target);
+			if (target.is("#sidebar *")) {
+				return;
+			}
+			if ($("#sidebar").hasClass("on")) {
+				scrollOff();
 			}
 		}
 	});
 	if (window.matchMedia("(min-width: 1100px)").matches) {
-		scrollOn();
-	} else {
-		scrollOff();
+		return scrollOn();
 	}
+	scrollOff();
 }
 
 //生成文章目录
@@ -68,47 +68,45 @@ function showArticleIndex() {
 	$(".article-toc.active-toc").removeClass("active-toc");
 	$("#tree .active").next().addClass("active-toc");
 
-	var labelList = $("#article-content").children();
-	var content = "<ul>";
-	var max_level = 4;
-	for (var i = 0; i < labelList.length; i++) {
-		var level = 5;
-		if ($(labelList[i]).is("h1")) {
-			level = 1;
-		} else if ($(labelList[i]).is("h2")) {
-			level = 2;
-		} else if ($(labelList[i]).is("h3")) {
-			level = 3;
-		} else if ($(labelList[i]).is("h4")) {
-			level = 4;
-		}
+	let labelList = $("#article-content").children();
+	let content = "<ul>";
+
+	let max_level = 4;
+	let labelLevel = {
+		h1: 1,
+		h2: 2,
+		h3: 3,
+		h4: 4,
+	};
+	let labelMaxLevel = {
+		h1: 1 - max_level + 1,
+		h2: 2 - max_level + 1,
+		h3: 3 - max_level + 1,
+		h4: 4 - max_level + 1,
+	};
+
+	for (let label of labelList) {
+		let level = labelLevel[$(label).prop("tagName").toLowerCase()] || 5;
 		if (level < max_level) {
 			max_level = level;
 		}
 	}
-	for (var i = 0; i < labelList.length; i++) {
-		var level = 0;
-		if ($(labelList[i]).is("h1")) {
-			level = 1 - max_level + 1;
-		} else if ($(labelList[i]).is("h2")) {
-			level = 2 - max_level + 1;
-		} else if ($(labelList[i]).is("h3")) {
-			level = 3 - max_level + 1;
-		} else if ($(labelList[i]).is("h4")) {
-			level = 4 - max_level + 1;
-		}
+
+	Array.from(labelList).forEach((label, i) => {
+		let level = labelMaxLevel[$(label).prop("tagName").toLowerCase()] || 0;
 		if (level != 0) {
-			$(labelList[i]).before('<span class="anchor" id="_label' + i + '"></span>');
+			$(label).before('<span class="anchor" id="_label' + i + '"></span>');
 			content +=
 				'<li class="level_' +
 				level +
-				'"><i class="fa fa-circle" aria-hidden="true"></i><a href="#_label' +
+				'"><a href="#_label' +
 				i +
 				'"> ' +
-				$(labelList[i]).text() +
+				$(label).text() +
 				"</a></li>";
 		}
-	}
+	});
+
 	content += "</ul>";
 
 	$(".article-toc.active-toc").append(content);
@@ -118,22 +116,21 @@ function showArticleIndex() {
 		$(".article-toc a").on("click", function (e) {
 			e.preventDefault();
 			// 获取当前点击的 a 标签，并前触发滚动动画往对应的位置
-			var target = $(this.hash);
+			let target = $(this.hash);
 			$("body, html").animate({ scrollTop: target.offset().top }, 500);
 		});
 
 		// 监听浏览器滚动条，当浏览过的标签，给他上色。
 		$(window).on("scroll", function (e) {
-			var anchorList = $(".anchor");
+			let anchorList = $(".anchor");
 			anchorList.each(function () {
-				var tocLink = $('.article-toc a[href="#' + $(this).attr("id") + '"]');
-				var anchorTop = $(this).offset().top;
-				var windowTop = $(window).scrollTop();
+				let tocLink = $('.article-toc a[href="#' + $(this).attr("id") + '"]');
+				let anchorTop = $(this).offset().top;
+				let windowTop = $(window).scrollTop();
 				if (anchorTop <= windowTop + 100) {
-					tocLink.addClass("read");
-				} else {
-					tocLink.removeClass("read");
+					return tocLink.addClass("read");
 				}
+				tocLink.removeClass("read");
 			});
 		});
 	}
@@ -142,18 +139,13 @@ function showArticleIndex() {
 }
 
 function pjaxLoad() {
-	$(document).pjax("#menu a", "#content", {
+	let pjaxOptions = {
 		fragment: "#content",
 		timeout: 8000,
-	});
-	$(document).pjax("#tree a", "#content", {
-		fragment: "#content",
-		timeout: 8000,
-	});
-	$(document).pjax("#index a", "#content", {
-		fragment: "#content",
-		timeout: 8000,
-	});
+	};
+	$(document).pjax("#menu a", "#content", pjaxOptions);
+	$(document).pjax("#tree a", "#content", pjaxOptions);
+	$(document).pjax("#index a", "#content", pjaxOptions);
 	$(document).on({
 		"pjax:complete": function (e) {
 			$("pre code").each(function (i, block) {
@@ -164,14 +156,14 @@ function pjaxLoad() {
 			//根据链接路径匹配标题路径
 			let full_title = decodeURI(window.location.pathname).slice(1, -1).split("/");
 			full_title.splice(0, 3);
-			var title = full_title.join("/");
+			let title = full_title.join("/");
 			if (title.length) {
-				var searchResult = $("#tree li.file").find("a[title='" + title + "']");
+				let searchResult = $("#tree li.file").find("a[title='" + title + "']");
 				if (searchResult.length) {
 					$(".fa-minus-square-o").removeClass("fa-minus-square-o").addClass("fa-plus-square-o");
 					$("#tree ul").css("display", "none");
 					if (searchResult.length > 1) {
-						var categorie = $("#article-categories span:last a").html();
+						let categorie = $("#article-categories span:last a").html();
 						if (typeof categorie != "undefined") {
 							categorie = categorie.trim();
 							searchResult = $("#tree li.directory a:contains('" + categorie + "')")
@@ -204,36 +196,31 @@ function serachTree() {
 		e.preventDefault();
 
 		// 获取 inpiut 输入框的内容
-		var inputContent = e.currentTarget.value;
+		let inputContent = e.currentTarget.value;
 
 		// 没值就收起父目录，但是得把 active 的父目录都展开
 		if (inputContent.length === 0) {
 			$(".fa-minus-square-o").removeClass("fa-minus-square-o").addClass("fa-plus-square-o");
 			$("#tree ul").css("display", "none");
 			if ($("#tree .active").length) {
-				showActiveTree($("#tree .active"), true);
-			} else {
-				$("#tree").children().css("display", "block");
+				return showActiveTree($("#tree .active"), true);
 			}
+			return $("#tree").children().css("display", "block");
 		}
 		// 有值就搜索，并且展开父目录
-		else {
-			$(".fa-plus-square-o").removeClass("fa-plus-square-o").addClass("fa-minus-square-o");
-			$("#tree ul").css("display", "none");
-			var searchResult = $("#tree li").find("a:contains('" + inputContent + "')");
-			if (searchResult.length) {
-				showActiveTree(searchResult.parent(), false);
-			}
+		$(".fa-plus-square-o").removeClass("fa-plus-square-o").addClass("fa-minus-square-o");
+		$("#tree ul").css("display", "none");
+		let searchResult = $("#tree li").find("a:contains('" + inputContent + "')");
+		if (searchResult.length) {
+			showActiveTree(searchResult.parent(), false);
 		}
 	});
 
 	$("#search-input").on("keyup", function (e) {
 		e.preventDefault();
-		if (event.keyCode == 13) {
-			var inputContent = e.currentTarget.value;
-
-			if (inputContent.length === 0) {
-			} else {
+		if (e.keyCode == 13) {
+			let inputContent = e.currentTarget.value;
+			if (!inputContent.length === 0) {
 				window.open(searchEngine + inputContent + "%20site:" + homeHost, "_blank");
 			}
 		}
@@ -243,7 +230,7 @@ function serachTree() {
 // 点击目录事件
 function clickTreeDirectory() {
 	// 判断有 active 的话，就递归循环把它的父目录打开
-	var treeActive = $("#tree .active");
+	let treeActive = $("#tree .active");
 	if (treeActive.length) {
 		showActiveTree(treeActive, true);
 	}
@@ -256,9 +243,9 @@ function clickTreeDirectory() {
 
 // 展开子节点
 function showActiveChildren(node) {
-	var icon = node.parent().find(".fa").first();
-	var iconIsOpen = icon.hasClass("fa-minus-square-o");
-	var subTree = node.siblings("ul");
+	let icon = node.parent().find(".fa").first();
+	let iconIsOpen = icon.hasClass("fa-minus-square-o");
+	let subTree = node.siblings("ul");
 
 	icon.removeClass("fa-minus-square-o").removeClass("fa-plus-square-o");
 
@@ -267,12 +254,13 @@ function showActiveChildren(node) {
 			subTree.slideUp({ duration: 100 });
 		}
 		icon.addClass("fa-plus-square-o");
-	} else {
-		if (typeof subTree != "undefined") {
-			subTree.slideDown({ duration: 100 });
-		}
-		icon.addClass("fa-minus-square-o");
+		return;
 	}
+
+	if (typeof subTree != "undefined") {
+		subTree.slideDown({ duration: 100 });
+	}
+	icon.addClass("fa-minus-square-o");
 }
 
 // 循环递归展开父节点
@@ -308,7 +296,7 @@ function showActiveTree(jqNode, isSiblings) {
 }
 
 function scrollOn() {
-	var $sidebar = $("#sidebar"),
+	let $sidebar = $("#sidebar"),
 		$content = $("#content"),
 		$header = $("#header"),
 		$footer = $("#footer"),
@@ -330,7 +318,7 @@ function scrollOn() {
 }
 
 function scrollOff() {
-	var $sidebar = $("#sidebar"),
+	let $sidebar = $("#sidebar"),
 		$content = $("#content"),
 		$header = $("#header"),
 		$footer = $("#footer"),
@@ -356,13 +344,13 @@ function wrapImageWithFancyBox() {
 	$("img")
 		.not("#header img")
 		.each(function () {
-			var $image = $(this);
-			var imageCaption = $image.attr("alt");
-			var $imageWrapLink = $image.parent("a");
+			let $image = $(this);
+			let imageCaption = $image.attr("alt");
+			let $imageWrapLink = $image.parent("a");
 
 			if ($imageWrapLink.length < 1) {
-				var src = this.getAttribute("src");
-				var idx = src.lastIndexOf("?");
+				let src = this.getAttribute("src");
+				let idx = src.lastIndexOf("?");
 				if (idx != -1) {
 					src = src.substring(0, idx);
 				}
@@ -380,5 +368,53 @@ function wrapImageWithFancyBox() {
 		thumbs: {
 			autoStart: false,
 		},
+	});
+}
+
+function toggleAll() {
+	let treeUls = $("#tree > ul");
+
+	treeUls.each(function () {
+		let beforeEl = $('<i class="fa fa-caret-down" aria-hidden="true"></i>');
+		beforeEl.css("position", "absolute");
+		beforeEl.css("right", "20px");
+		beforeEl.css("cursor", "pointer");
+		beforeEl.css("color", "#454545");
+		beforeEl.hover(
+			function () {
+				beforeEl.css("color", "rgb(122, 122, 122)");
+			},
+			function () {
+				beforeEl.css("color", "#454545");
+			}
+		);
+		$(this).prepend(beforeEl);
+
+		beforeEl.on("click", function () {
+			let branches = $(this).parent().find("a.directory i");
+
+			if ($(this).hasClass("fa-caret-down")) {
+				$(this).removeClass("fa-caret-down");
+				$(this).addClass("fa-caret-up");
+
+				branches.each(function () {
+					if ($(this).hasClass("fa-minus-square-o")) {
+						return;
+					}
+					$(this).click();
+				});
+				return;
+			}
+
+			$(this).removeClass("fa-caret-up");
+			$(this).addClass("fa-caret-down");
+
+			branches.each(function () {
+				if ($(this).hasClass("fa-plus-square-o")) {
+					return;
+				}
+				$(this).click();
+			});
+		});
 	});
 }
